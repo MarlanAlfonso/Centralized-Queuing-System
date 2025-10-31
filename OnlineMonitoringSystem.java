87303f296d2b1964e1bc4f969a81bd159aee274e
Marlan Alfonso <alfonso.marlan1184@gmail.com>
2025-10-31 12:04:43 +0800

    Add files via upload

---
+++ OnlineMonitoringSystem.java
@@ -0,0 +1,13 @@
+package pagibigqueue;
+
+public class OnlineMonitoringSystem {
+    private QueueManager queueManager;
+
+    public OnlineMonitoringSystem() {
+        this.queueManager = QueueManager.getInstance();
+    }
+
+    public void displayCurrentNumber() {
+        System.out.println("Currently serving number: " + queueManager.getCurrentNumber());
+    }
+}
---
+++ QueueManager.java
@@ -0,0 +1,28 @@
+package pagibigqueue;
+
+public class QueueManager {
+    private static QueueManager instance;
+    private int currentNumber = 0;
+
+    private QueueManager() {}
+
+    public static QueueManager getInstance() {
+        if (instance == null) {
+            instance = new QueueManager();
+        }
+        return instance;
+    }
+
+    public int getNextNumber() {
+        return ++currentNumber;
+    }
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public void resetNumber(int number) {
+        this.currentNumber = number;
+        System.out.println("Queue number reset to: " + number);
+    }
+}
